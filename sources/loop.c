b950b358cb898c08584f8c5a378f2aa2ad25a37f
Benian Toksöz <benian@172-15-184-47.lightspeed.wepbfl.sbcglobal.net>
2024-03-31 16:27:19 +0100

    started mini_cd, will continue from new branch

--- sources/loop.c
+++ sources/loop.c
@@ -15,27 +15,27 @@ void	loop(t_shell *shell)
 			break;
 		if (*shell->line)
 			add_history(shell->line);
-		// if (ft_strcmp(shell->line, "exit") == 0)
-		// 	break ;
+		if (ft_strcmp(shell->line, "exit") == 0)
+			break ;
 		if (check_syntax_errors(shell->line))
 			continue ;
 		shell->tokens = tokenize(shell->line);
 		if (check_tokens(shell->tokens))
 			continue ;
 		expand(shell->tokens);
-		// print_tokens(shell->tokens);
+		print_tokens(shell->tokens);
 		if (!shell->tokens)
 			status = 1; 								// search for the right status value
 		if (!status)
 			shell->tree = parse_commandline(shell->tokens);
-		// quick test of echo
-		while (shell->tree)
-		{
-			if (shell->tree->builtin != NULL)
-				shell->tree->builtin(shell, shell->tree);
-			shell->tree = shell->tree->left;
-		}
-		// print_tree(shell->tree, 0);
+		// // quick test of echo
+		// while (shell->tree)
+		// {
+		// 	if (shell->tree->builtin != NULL)
+		// 		shell->tree->builtin(shell, shell->tree);
+		// 	shell->tree = shell->tree->left;
+		// }
+		print_tree(shell->tree, 0);
 		// execute(shell->tree);
 		// reset()	//reset lists of tokens etc, but keep history
 		// free(shell->line);
--- sources/mini_cd.c
+++ sources/mini_cd.c
@@ -1,8 +1,16 @@
 #include "../includes/minishell.h"
 
+/* with no args: change envp->pwd to envp->home
+with a given path: extend path to make it absolute then update pwd and olpwd
+if first argument is -: change to pwd to OLDPWD
+also implement ..?
+*/
 int	mini_cd(t_shell *shell, t_tree_node *tree)
 {
 	//TODO
+	//get current path
+		//shell->envp, somehow find PWD, OLDPWD, HOME
+	//wi
 	(void)shell;
 	(void)tree;
 	return (1);
