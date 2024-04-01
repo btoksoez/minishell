e1fbc0652a993ed76fc682804475b5ba3d446771
Benian Toksoez <btoksoez@c2r6s3.42lisboa.com>
2024-04-01 17:11:26 +0100

    started mini_export

--- sources/mini_export.c
+++ sources/mini_export.c
@@ -1,5 +1,10 @@
 #include "../includes/minishell.h"
 
+/* if no args: show all env, (but sorted?)
+for each arg: 
+	check if equal sign:
+		no: add to envp with =''
+		yes: add with value after= */
 int	mini_export(t_shell *shell, t_tree_node *tree)
 {
 	//TODO
