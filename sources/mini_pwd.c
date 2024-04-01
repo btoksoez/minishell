fa53e0bce39782c76bbec3f2b8128d40a2a94ab2
Benian Toksoez <btoksoez@c2r6s3.42lisboa.com>
2024-04-01 13:07:08 +0100

    mini_pwd done

--- sources/mini_pwd.c
+++ sources/mini_pwd.c
@@ -1,9 +1,20 @@
 #include "../includes/minishell.h"
 
-int	mini_pwd(t_shell *shell, t_tree_node *tree)
+int	mini_pwd(t_shell *s, t_tree_node *tree)
 {
-	//TODO
-	(void)shell;
+	int	i;
+
+	i = 0;
+	while (s->envp[i])
+	{
+		if (ft_strncmp(s->envp[i], "PWD=", 4) == 0)
+		{
+			s->envps->pwd_index = i;
+			s->envps->pwd = s->envp[i] + 4;
+		}
+		i++;
+	}
+	ft_putendl_fd(s->envps->pwd, STDOUT_FILENO);
 	(void)tree;
-	return (1);
+	return (EXIT_SUCCESS);
 }
