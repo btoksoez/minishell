c7a4237bf14ed7bf00c8b59503aca59b088ed5ee
Benian Toksoez <btoksoez@c2r6s3.42lisboa.com>
2024-04-01 17:01:48 +0100

    mini_unset done, added function to libft

--- includes/libft/ft_strequ.c
+++ includes/libft/ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: btoksoez <btoksoez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 14:50:23 by btoksoez          #+#    #+#             */
-/*   Updated: 2024/04/01 14:53:29 by btoksoez         ###   ########.fr       */
+/*   Updated: 2024/04/01 16:51:14 by btoksoez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ int	ft_strequ(const char *s1, const char *s2)
 {
 	if (!s1 || !s2)
 		return (0);
-	while (*s1 && *s2)
+	while (*s1 && *s2 && *s1 != '=')
 	{
 		if (*s1 != *s2)
 			return (0);
 		s1++;
 		s2++;
 	}
-	return (*s1 == '\0' && *s2 == '\0');
+	if ((*s1 == '=' && *s2 == '\0') || (*s1 == '\0' && *s2 == '\0'))
+		return (1);
+	return (0);
 }
 // int main(void)
 // {
-// 	char *s1 = NULL;
-// 	char *s2 = NULL;
+// 	char *s1 = "PWD=/dhdhwdjwd";
+// 	char *s2 = "PWD";
 
 // 	printf("%d", ft_strequ(s1, s2));
 // }
--- sources/mini_unset.c
+++ sources/mini_unset.c
@@ -10,7 +10,6 @@ void	remove_entry_from_envp(char ***envp, int entry_index)
 	int	j;
 	char	**new_envp;
 
-
 	len = 0;
 	i = 0;
 	j = 0;
@@ -19,18 +18,11 @@ void	remove_entry_from_envp(char ***envp, int entry_index)
 	if (entry_index >= len || entry_index < 0)
 		return (error_message("remove entry error"));
 	new_envp = malloc(sizeof(char *) * len);
-	while (j < entry_index)
+	while (j < len - 1)
     {
-        new_envp[j] = (*envp)[j];
-        j++;
-    }
-
-    // Skip the removed entry and continue copying
-    i = entry_index + 1;
-    while (i < len)
-    {
-        new_envp[j] = (*envp)[i];
-        i++;
+		if (j == entry_index)
+			i = 1;
+        new_envp[j] = (*envp)[i + j];
         j++;
     }
 	new_envp[j] = NULL;
@@ -42,19 +34,13 @@ int	mini_unset(t_shell *s, t_tree_node *tree)
 {
 	int	i;
 
-	mini_env(s, tree);
-	i = 0;
-	while (s->envp[i])
-		i++;
-	printf("Starting len: %d\n", i);
 	while (tree->args)
 	{
 		i = 0;
 		while (s->envp[i])
 		{
-			if (!ft_strncmp(s->envp[i], tree->args->arg, ft_strlen(tree->args->arg)))
+			if (ft_strequ(s->envp[i], tree->args->arg))
 			{
-				printf("removing entry: %s\n", s->envp[i]);
 				remove_entry_from_envp(&(s->envp), i);
 				continue;	//don't increase i to stay at right position
 			}
@@ -62,12 +48,5 @@ int	mini_unset(t_shell *s, t_tree_node *tree)
 		}
 		tree->args = tree->args->next;
 	}
-	mini_env(s, tree);
-	i = 0;
-	while (s->envp[i])
-		i++;
-	printf("Final len: %d\n", i);
-	if (s->envp[i] == NULL)
-		printf("\nis null terminated\n");
 	return (1);
 }
