808b1089189cb196430feed12207f3c4c4420236
Benian Toksöz <benian@172-15-184-73.lightspeed.wepbfl.sbcglobal.net>
2024-04-09 13:32:57 +0100

    fixed exit and other stuff, but exit is not executed multiple times with pipes

--- sources/3.builtins/mini_exit.c
+++ sources/3.builtins/mini_exit.c
@@ -12,13 +12,18 @@ int	is_digit_string(char *str)
 int	mini_exit(t_shell *shell, t_tree_node *tree)
 {
 	int	sign;
+	int	exit_flag;
 
 	sign = 1;
-	(void)tree;
+	exit_flag = 1;
+	if (shell->pipe_nbr > 0)
+		exit_flag = 0;
 	if (!tree->args)
 	{
 		shell->status = 0;
-		clean_up(shell, 1);
+		if (exit_flag)
+			clean_up(shell, 1);
+		return (EXIT_SUCCESS);
 	}
 	if (!ft_strcmp("+", tree->args->arg) || !ft_strcmp("-", tree->args->arg))
 	{
@@ -26,20 +31,22 @@ int	mini_exit(t_shell *shell, t_tree_node *tree)
 			sign = -1;
 		tree->args = tree->args->next;
 	}
+	if (!is_digit_string(tree->args->arg))
+	{
+		ft_putstr_fd("exit\nminishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd(tree->args->arg, STDERR_FILENO);
+		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+		shell->status = 255;
+		if (exit_flag)
+			clean_up(shell, 0);
+	}
 	if (tree->args->next)
 	{
 		shell->status = 1;
 		return (error_message("exit\nminishell: exit: too many arguments"), EXIT_FAILURE);
 	}
-	if (!is_digit_string(tree->args->arg))
-	{
-		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-		ft_putstr_fd(tree->args->arg, STDERR_FILENO);
-		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-		shell->status = 255;
+	shell->status = (sign * ft_atoi(tree->args->arg)) % 256;
+	if (exit_flag)
 		clean_up(shell, 1);
-	}
-	shell->status = sign * ft_atoi(tree->args->arg) % 256;
-	clean_up(shell, 1);
-	return (0);
+	return (shell->status);
 }
