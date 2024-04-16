7078b9b784f91992f7f7893a562dc02763f555a0
Benian Toksoez <btoksoez@c2r8s5.42lisboa.com>
2024-04-16 17:42:18 +0100

    fixed export leaks

--- sources/3.builtins/mini_export.c
+++ sources/3.builtins/mini_export.c
@@ -140,8 +140,8 @@ void	append_env(char **s, char *var, char *value)
 		free(current_value);
 	free(append_value);
 	free(new_value);
-	free(var);
 	free(new_var);
+	free(value);
 }
 
 /* if append flag, it should find var*/
@@ -257,7 +257,7 @@ int	mini_export(t_shell *s, t_tree_node *tree)
 {
 	char	*var;
 	char	*value;
-	// char	*new;
+	char	*new_value;
 	char	**copy;
 	char	*new_var;
 	int		flag_append;
@@ -276,6 +276,7 @@ int	mini_export(t_shell *s, t_tree_node *tree)
 	if (!tree->args)
 	{
 		print_env(copy);
+		ft_freematrix(copy);
 		return (EXIT_SUCCESS);
 	}
 	current = tree->args;
@@ -293,6 +294,7 @@ int	mini_export(t_shell *s, t_tree_node *tree)
 			if (current)
 				current = current->next;
 			return_value = 1;
+			free(var);
 			continue;
 		}
 		//check for equal sign
@@ -301,18 +303,22 @@ int	mini_export(t_shell *s, t_tree_node *tree)
 			//check if it's in envp, if not then add it, else nothing
 			if (!is_inenvp(s->envp, var))
 				add_env(s, var);
+			else
+				free(var);
 		}
 		else
 		{
-			value = ft_strdup(ft_strchr(current->arg, '=') + 1);
+			new_value = ft_strdup(ft_strchr(current->arg, '=') + 1);
 			new_var = ft_strjoin(var, "=");
-			value = ft_strjoin(new_var, value);
+			value = ft_strjoin(new_var, new_value);
+			free(new_value);
 			free(new_var);
 			//check if var in s->envp already, if yes replace, else add
 			if (is_inenvp(s->envp, var))
 				replace_env(s, var, value, flag_append);
 			else
 				add_env(s, value);
+			free(var);
 		}
 		if (current)
 			current = current->next;
